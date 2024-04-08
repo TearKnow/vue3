210da78bb91323b12a9c60a6418cf466e19ab1cf
Jack <904916790@qq.com>
2024-04-08 22:39:08 +0800

    1. add uuid module  2. use ref

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="red">{{myname}}</div>
-
+    <div>例子1: ref进行响应式更新（比如每隔几秒更新下）</div>
+    <div>这是用正常思路做的，每隔几秒有新值，但是界面是不会变的：{{myRandomValue}}</div>
+    <div>这是用了ref响应式的, <span class="important">要用const，然后修改value才行。这里npm i uuid安装了uuid模块</span>：{{ uuidRef }}</div>
 </template>
 
 
 <script setup>
-  const myname = 'jack'
+  import { ref } from 'vue'
+  import { v4 as uuid } from 'uuid'
+  //例子1: 用如下代码，虽然random的会更新，但是页面是不会变的
+  let myRandomValue = Math.random()
+  setInterval(() => {
+    myRandomValue = Math.random()
+    console.log(myRandomValue)
+  }, 10000)
+  //例子1: 正确用法
+  const uuidRef = ref(uuid())
+  setInterval(() => uuidRef.value = uuid(), 1000)
+
+
+   
+
+
+
 </script>
 
 <style>
-  .red{
+  .important{
     color: red;
+    font-weight: bold;
   }
 </style>
