aa01294531d1814f371aee1bb52f6c52c1f80f9c
Jack <904916790@qq.com>
2024-04-08 22:03:43 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>jackkk</div>
+  <div class="red">{{myname}}</div>
+
 </template>
+
+
+<script setup>
+  const myname = 'jack'
+</script>
+
+<style>
+  .red{
+    color: red;
+  }
+</style>
