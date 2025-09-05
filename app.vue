af61cf2e857b12bd78337ed37d4945db8bdf8b13
Jack <904916790@qq.com>
2025-09-05 23:15:46 +0800

    use vant

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <NuxtPage />
+
+
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+
+
 </template>
 
 
@@ -8,5 +17,8 @@ useSeoMeta({
   title: "bilibili",
   description: "b站，你的选择",
 })
+
+
+showSuccessToast('成功文案');
 
 </script>
