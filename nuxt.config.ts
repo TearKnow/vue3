730ee82111c1baa2eaa8419cff9f39b21fac06d9
Jack <904916790@qq.com>
2025-12-27 22:28:31 +0800

    can cors, need use tun mode in clash verge !!!!!!!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@vant/nuxt'],
+  nitro: {
+    //这种方法直接通过代码层面强行把流量塞进代理端口
+    devProxy: {
+      '/api': {
+        target: 'https://www.jjshouse.com/api',
+      }
+    },
+    routeRules: {
+      '/api/**': {
+        proxy: 'https://www.jjshouse.com/api/**',
+      }
+    }
+  }
 })
--- pages/test_proxy.vue
+++ pages/test_proxy.vue
@@ -1,12 +1,7 @@
 <script setup>
 // 使用 useFetch 请求
 // 这里的 '/api/user' 实际上会被转发到 'https://external-api.com/user'
-const { data, pending, error, refresh } = await useFetch('/api/user', {
-  // 即使在 config 里配了，这里也可以手动再传一次关键 Header
-  headers: {
-    'Accept-Language': 'en-US,en;q=0.9',
-  },
-  // 确保在服务器端执行时也能正确处理代理
+const { data, pending, error, refresh } = await useFetch('/api/v2/common/nav/header', {
   onRequest({ request, options }) {
     console.log('正在请求:', request)
   },
@@ -27,19 +22,3 @@ const { data, pending, error, refresh } = await useFetch('/api/user', {
 </template>
 
 
-import { HttpsProxyAgent } from 'https-proxy-agent'
-
-export default defineNuxtConfig({
-compatibilityDate: '2024-11-01',
-devtools: { enabled: true },
-modules: ['@vant/nuxt'],
-nitro: {
-// 这种方法直接通过代码层面强行把流量塞进代理端口
-devProxy: {
-'/api': {
-target: 'https://www.jjshouse.com/api',
-agent: new HttpsProxyAgent('http://127.0.0.1:7897') // 你的 Clash 端口
-}
-}
-}
-})
