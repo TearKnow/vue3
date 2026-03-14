eef6b67dcb57d23a4ae3dda4fe43f568e6a05697
Jack <904916790@qq.com>
2026-03-14 17:49:24 +0800

    tech: use try catch

---
+++ pages/tech/001try.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    use try catch
+  </div>
+</template>
+
+<script>
+try {
+  throw { error: 1, msg: 'something wrong' }
+}
+catch (error) {
+  console.log(error)
+}
+</script>
