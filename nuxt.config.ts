02d6f6d3c5ce9de1b0bee27b1a77365633e42292
Jack <904916790@qq.com>
2026-03-14 12:43:29 +0800

    my code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
   modules: ['@vant/nuxt'],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-11-01',
   nitro: {
-    //这种方法直接通过代码层面强行把流量塞进代理端口
+    // 这种方法直接通过代码层面强行把流量塞进代理端口
     devProxy: {
       '/api': {
         target: 'https://www.jjshouse.com/api',
-      }
+      },
     },
     routeRules: {
       '/api/**': {
         proxy: 'https://www.jjshouse.com/api/**',
-      }
-    }
-  }
+      },
+    },
+  },
 })
