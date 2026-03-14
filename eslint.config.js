33a62bc182a01a86e60976fbb48dec297a5749d7
Jack <904916790@qq.com>
2026-03-14 12:38:05 +0800

    code format: not error when not used param

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,14 @@ import { createConfigForNuxt } from '@nuxt/eslint-config'
 
 export default createConfigForNuxt({
   features: {
-    stylistic: true, // 开启格式化规则（包括缩进）
+    stylistic: true,
+  },
+}).append({
+  rules: {
+    // 把未使用变量从错误降级为警告
+    // 'no-unused-vars': 'warn',
+    // 或者如果你想完全关闭（不推荐）
+    'no-unused-vars': 'off',
+
   },
 })
