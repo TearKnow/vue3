1c06aa3927422c57b599b1e0d42099890be0a46d
Jack <904916790@qq.com>
2026-03-21 16:28:33 +0800

    tech: 002 promise

--- pages/tech/002promise.vue
+++ pages/tech/002promise.vue
@@ -1,10 +1,37 @@
 <template>
   <div>
-    check console
+    demo1 & demo2: check vue file
+    the alert is demo3
+
+    <van-popup
+      v-model:show="visible"
+      position="center"
+      :close-on-click-overlay="false"
+    >
+      <div class="confirm-dialog">
+        <div class="content">
+          {{ title }}
+        </div>
+        <div class="btns">
+          <van-button
+            type="default"
+            @click="handleCancel"
+          >
+            取消
+          </van-button>
+          <van-button
+            type="primary"
+            @click="handleOk"
+          >
+            确认
+          </van-button>
+        </div>
+      </div>
+    </van-popup>
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 // demo1
 /*
 const promise = new Promise((resolve, reject) => {
@@ -22,6 +49,8 @@ promise.then(
 /**
  * 这里await等待执行完，后面的才能执行
  */
+
+/*
 const waitFor = function () {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -39,7 +68,51 @@ const f1 = async () => {
 
 f1().then(
   (value) => {
-    console.log('now get ajax result ' + value)
+    console.log('now get ajax result ====> ' + value)
   },
 )
+*/
+
+// demo3.基于 Promise 的自定义确认弹窗（等待用户点击 OK/Cancel 后返回结果）全程可直接运行
+
+const visible = ref(false)
+
+// 存储 Promise 回调
+let resolveCallback: (result: boolean) => void = () => {}
+
+const title = ref('')
+
+// 打开弹窗（返回 Promise）
+const openDialog = () => {
+  visible.value = true
+  return new Promise<boolean>((resolve) => {
+    resolveCallback = resolve
+  })
+}
+
+// 点击确认
+const handleOk = () => {
+  visible.value = false
+  resolveCallback(true)
+}
+
+// 点击取消
+const handleCancel = () => {
+  visible.value = false
+  resolveCallback(false)
+}
+
+// 页面加载自动弹出
+onMounted(async () => {
+  // 等待用户选择
+  const result = await openDialog()
+
+  // 输出结果
+  if (result) {
+    showToast('你选择了：OK 确认')
+  }
+  else {
+    showToast('你选择了：Cancel 取消')
+  }
+})
 </script>
