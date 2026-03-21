8c60b8d9e8961ece33b5d4f824023d7a41d1ffb0
Jack <904916790@qq.com>
2026-03-21 15:47:49 +0800

    config: 忽略误飘红

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,9 @@ export default createConfigForNuxt({
   },
 }).append({
   rules: {
+    // Nuxt auto-import APIs (e.g. useSeoMeta) are injected at build time.
+    // Disable no-undef to avoid false positives in <script setup>.
+    'no-undef': 'off',
     // 把未使用变量从错误降级为警告
     // 'no-unused-vars': 'warn',
     // 或者如果你想完全关闭（不推荐）
