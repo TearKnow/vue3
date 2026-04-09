57d6beb3ad10fc206734968bace06eff5b3720c8
dunpai <dunpai@kerrylan.com>
2026-04-09 16:10:35 +0800

    tech: learn defineModel

---
+++ pages/tech/defineModel/parent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    案例：学习defineModel的使用，在父组件中改了，子组件会变。子组件中改了，父组件会变。
+    <hr>
+    parentInput:
+    <input type="text" v-model="selectTopicParentKey">
+
+    <Son v-model:parent-key="selectTopicParentKey" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Son from './son.vue'
+
+const selectTopicParentKey = ref('')
+</script>
---
+++ pages/tech/defineModel/son.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    sonInput:
+    <input type="text" v-model="select1">
+  </div>
+</template>
+
+<script setup>
+const select1 = defineModel('parentKey', { default: '' })
+</script>
