fea01a44f42f765da1748e3f79fef627b82a80d8
Jack <904916790@qq.com>
2026-04-21 22:11:15 +0800

    demno

--- pages/tech/005watch.vue
+++ pages/tech/005watch.vue
@@ -29,6 +29,60 @@
         </b>
       </div>
     </div>
+
+    <div class="demo-block">
+      <h2>用法3: watch 监视reactive定义的【对象类型】数据</h2>
+
+      <div>
+        address:{{ address.city }} <br>
+
+        zipcode:{{ address.zipcode }} <br>
+        <button @click="changeAddress">
+          changeAddress
+        </button>
+        <button @click="changeZipcode">
+          changeZipcode
+        </button>
+        <button @click="changeAll">
+          changeAll
+        </button>
+        <div class="demo-tip">
+          reactive的默认行为是深度监听
+        </div>
+      </div>
+    </div>
+
+    <div class="demo-block">
+      <h2>用法4: 监视ref或者reactive 定义的【对象类型】数据中的某个属性</h2>
+
+      <div>
+        公司名字：{{ company.name }} <br>
+        公司地址：{{ company.location }} <br>
+        公司资产：{{ company.car.c1 }} 、{{ company.car.c2 }} <br>
+      </div>
+
+      <button @click="changeCompanyName">
+        修改公司名字
+      </button>
+      <button @click="changeCompanyCar1">
+        修改公司某个资产
+      </button>
+      <button @click="changeCompanyCar">
+        修改公司所有资产
+      </button>
+
+      <div class="demo-tip">
+        <div>若该属性不是【对象类型】，需要写成函数形式</div>
+        <div>若该属性是【对象类型】，可以写，也可以写成函数形式，不过建议写成函数形式，记起来方便，最多价格deep</div>
+      </div>
+    </div>
+
+    <div>
+      <h2>情况5: 监视上述的多个数据</h2>
+      <div class="demo-tip">
+        就是watch的时候放到一个数组里，监听多个数据
+      </div>
+    </div>
   </div>
 </template>
 
@@ -69,5 +123,60 @@ watch(
     console.log('person 变了', oldValue, 'to', newValue)
   },
   { deep: true, immediate: true },
+)
+
+const address = reactive({
+  city: 'New York',
+  zipcode: '10001',
+})
+function changeAddress() {
+  address.city = 'Los Angeles'
+}
+function changeZipcode() {
+  address.zipcode = '90001'
+}
+function changeAll() {
+  Object.assign(address, {
+    city: 'Chicago',
+    zipcode: '60601',
+  })
+}
+watch(
+  address,
+  (newValue, oldValue) => {
+    console.log('address 变了', oldValue, 'to', newValue)
+  },
+)
+
+const company = reactive({
+  name: 'Google',
+  location: 'Mountain View',
+  car: {
+    c1: 'BMW',
+    c2: 'Audi',
+  },
+})
+function changeCompanyName() {
+  company.name += '~'
+}
+function changeCompanyCar() {
+  company.car.c1 = 'Mercedes'
+  company.car.c2 = 'Lexus'
+}
+function changeCompanyCar1() {
+  company.car.c1 = 'Toyota'
+}
+watch(
+  () => company.name,
+  (newValue, oldValue) => {
+    console.log('company.name 变了', oldValue, 'to', newValue)
+  },
+)
+watch(
+  () => company.car,
+  (newValue, oldValue) => {
+    console.log('company.car 变了', oldValue, 'to', newValue)
+  },
+  { deep: true },
 )
 </script>
