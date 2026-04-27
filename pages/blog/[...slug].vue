5fea0a974b4eea587f2a6d06bf2b2e49a1d853b5
jack <904716790@qq.com>
2026-04-27 21:30:27 +0800

    remove radius

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1348,6 +1348,8 @@ watchEffect(() => {
   .blog-article article {
     border-top-left-radius: 0;
     border-top-right-radius: 0;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
   }
 
   .toc-toggle {
